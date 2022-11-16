ed09eb46d32d275b9c4fe9192dc41a0213f2ad34
Radoslav Čerhák <r.cerhak@gmail.com>
2022-11-16 14:14:17 +0100

    add missing annotation and modules

--- registrar-persistence/src/main/java/cz/metacentrum/registrarpersistence/repository/FormRepository.java
+++ registrar-persistence/src/main/java/cz/metacentrum/registrarpersistence/repository/FormRepository.java
@@ -2,6 +2,8 @@ package cz.metacentrum.registrarpersistence.repository;
 
 import cz.metacentrum.registrarpersistence.entity.Form;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FormRepository extends JpaRepository<Form, Long> {
 }
