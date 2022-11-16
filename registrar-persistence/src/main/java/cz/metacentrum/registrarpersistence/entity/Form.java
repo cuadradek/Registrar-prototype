9b86e325b5678c30b7d1ff1a7074c80689fad32f
Radoslav Čerhák <r.cerhak@gmail.com>
2022-11-16 14:05:51 +0100

    added getters, constructors to Form

--- registrar-persistence/src/main/java/cz/metacentrum/registrarpersistence/entity/Form.java
+++ registrar-persistence/src/main/java/cz/metacentrum/registrarpersistence/entity/Form.java
@@ -58,4 +58,117 @@ public class Form {
 	@OneToMany(mappedBy = "form")
 	private List<AssignedFormModule> assignedModules;
 
+	public Form() {
+	}
+
+	public Form(Long id, UUID idmObject, UUID idmFormManagersGroup, String name, String redirectUrl, List<Form> redirectForms, List<Form> autosendForms, boolean canBeResubmitted, boolean autoApprove, List<Form> nestedForms, List<ApprovalGroup> approvalGroup, List<AssignedFormModule> assignedModules) {
+		this.id = id;
+		this.idmObject = idmObject;
+		this.idmFormManagersGroup = idmFormManagersGroup;
+		this.name = name;
+		this.redirectUrl = redirectUrl;
+		this.redirectForms = redirectForms;
+		this.autosendForms = autosendForms;
+		this.canBeResubmitted = canBeResubmitted;
+		this.autoApprove = autoApprove;
+		this.nestedForms = nestedForms;
+		this.approvalGroup = approvalGroup;
+		this.assignedModules = assignedModules;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public UUID getIdmObject() {
+		return idmObject;
+	}
+
+	public void setIdmObject(UUID idmObject) {
+		this.idmObject = idmObject;
+	}
+
+	public UUID getIdmFormManagersGroup() {
+		return idmFormManagersGroup;
+	}
+
+	public void setIdmFormManagersGroup(UUID idmFormManagersGroup) {
+		this.idmFormManagersGroup = idmFormManagersGroup;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRedirectUrl() {
+		return redirectUrl;
+	}
+
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
+
+	public List<Form> getRedirectForms() {
+		return redirectForms;
+	}
+
+	public void setRedirectForms(List<Form> redirectForms) {
+		this.redirectForms = redirectForms;
+	}
+
+	public List<Form> getAutosendForms() {
+		return autosendForms;
+	}
+
+	public void setAutosendForms(List<Form> autosendForms) {
+		this.autosendForms = autosendForms;
+	}
+
+	public boolean isCanBeResubmitted() {
+		return canBeResubmitted;
+	}
+
+	public void setCanBeResubmitted(boolean canBeResubmitted) {
+		this.canBeResubmitted = canBeResubmitted;
+	}
+
+	public boolean isAutoApprove() {
+		return autoApprove;
+	}
+
+	public void setAutoApprove(boolean autoApprove) {
+		this.autoApprove = autoApprove;
+	}
+
+	public List<Form> getNestedForms() {
+		return nestedForms;
+	}
+
+	public void setNestedForms(List<Form> nestedForms) {
+		this.nestedForms = nestedForms;
+	}
+
+	public List<ApprovalGroup> getApprovalGroup() {
+		return approvalGroup;
+	}
+
+	public void setApprovalGroup(List<ApprovalGroup> approvalGroup) {
+		this.approvalGroup = approvalGroup;
+	}
+
+	public List<AssignedFormModule> getAssignedModules() {
+		return assignedModules;
+	}
+
+	public void setAssignedModules(List<AssignedFormModule> assignedModules) {
+		this.assignedModules = assignedModules;
+	}
 }
