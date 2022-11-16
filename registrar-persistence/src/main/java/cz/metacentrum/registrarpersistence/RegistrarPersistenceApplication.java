d2804a7c041aca4c6de873c743beb9fabe84240e
Radoslav Čerhák <r.cerhak@gmail.com>
2022-11-16 09:40:13 +0100

    introducing registrar-persistence

---
+++ registrar-persistence/src/main/java/cz/metacentrum/registrarpersistence/RegistrarPersistenceApplication.java
@@ -0,0 +1,13 @@
+package cz.metacentrum.registrarpersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarPersistenceApplication.class, args);
+	}
+
+}
---
+++ registrar-persistence/src/test/java/cz/metacentrum/registrarpersistence/RegistrarPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.metacentrum.registrarpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrarPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
