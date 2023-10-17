3d262ab29fa6cb03e631901f9b1f2754281fb359
Radoslav Čerhák <r.cerhak@gmail.com>
2023-10-17 12:07:31 +0200

    disable security for local profile

--- registrar-rest/src/main/java/cz/metacentrum/registrar/rest/config/LocalSecurityConfig.java
+++ registrar-rest/src/main/java/cz/metacentrum/registrar/rest/config/LocalSecurityConfig.java
@@ -1,7 +1,5 @@
 package cz.metacentrum.registrar.rest.config;
 
-import cz.metacentrum.registrar.service.RoleService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,23 +13,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Profile("!local")
+@Profile("local")
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class RegistrarSecurityConfig {
-
-	@Value("${spring.security.oauth2.resourceserver.opaquetoken.introspection-uri}")
-	private String introspectionUri;
-
-	@Value("${spring.security.oauth2.resourceserver.opaquetoken.client-id}")
-	private String clientId;
-
-	@Value("${spring.security.oauth2.resourceserver.opaquetoken.client-secret}")
-	private String clientSecret;
+public class LocalSecurityConfig {
 
 	@Bean
 	public RoleHierarchy roleHierarchy() {
@@ -54,34 +42,16 @@ public class RegistrarSecurityConfig {
 		http
 				.authorizeHttpRequests(
 						authorize -> authorize
-//								.requestMatchers("/swagger-ui/**").permitAll()
-//								.requestMatchers("/v3/api-docs/**").permitAll()
-//								.requestMatchers("/submissions/**").permitAll()
-//								.requestMatchers("/submitted-forms/**").permitAll()
-//								.requestMatchers("/forms/**").permitAll()
-//								.requestMatchers("/test/**").hasAuthority("SCOPE_openid")
-//								//if the request didn't match test/**, then try to match this:
-//								.requestMatchers("/forms/**").hasAuthority("SCOPE_REGISTRAR_API")
-								// if the request didn't match any ant matcher, then user needs to be at least authenticated
-								.anyRequest().authenticated()
+								.requestMatchers("/swagger-ui/**").permitAll()
+								.requestMatchers("/v3/api-docs/**").permitAll()
+								.requestMatchers("/submissions/**").permitAll()
+								.requestMatchers("/submitted-forms/**").permitAll()
+								.requestMatchers("/forms/**").permitAll()
+								.requestMatchers("/test/**").hasAuthority("SCOPE_openid")
 				)
-//				.oauth2ResourceServer(oauth2 -> oauth2.opaqueToken
-//						(token -> token.introspectionUri(this.introspectionUri)
-//								.introspectionClientCredentials(this.clientId, this.clientSecret)
-//								.introspector(introspector()))
-//				);
-//				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::opaqueToken)
 				.csrf(AbstractHttpConfigurer::disable)
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
-	}
-
-	@Bean
-	public OpaqueTokenIntrospector introspector(RoleService roleService) {
-		return new GoogleTokenIntrospector(introspectionUri, roleService);
-//		return new RegistrarTokenIntrospector(introspectionUri, clientId, clientSecret, roleService);
-//		return new SpringOpaqueTokenIntrospector(introspectionUri, clientId, clientSecret);
-//		return new NimbusOpaqueTokenIntrospector(introspectionUri, clientId, clientSecret);
 	}
 }
